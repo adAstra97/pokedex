d2ae4d76e386b9428119b1067ed93b6410fa3730
adAstra97 <131522998+adAstra97@users.noreply.github.com>
2024-11-05 18:36:24 +0300

    feat: add header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,11 @@
+import { Link } from "react-router-dom";
+
 export const Header: React.FC = () => {
-  return <div>Header</div>;
+  return (
+    <header className="header">
+      <div className="wrapper">
+        <Link to='/' className="header__logo">Pokedex</Link>
+      </div>
+    </header>
+  );
 };
