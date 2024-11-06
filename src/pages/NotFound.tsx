0b9a982e97ae7417b863a14c237b720563702473
adAstra97 <131522998+adAstra97@users.noreply.github.com>
2024-11-07 00:35:16 +0300

    feat: add not found page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,5 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export const NotFound: React.FC = () => {
-  return <div>NotFound</div>;
+  return (
+    <div className="wrapper">
+      <div className="not-found">
+        <h1>404</h1>
+        <h2>Page Not Found</h2>
+        <Link to="/" className="not-found__link">
+          Go back to the homepage
+        </Link>
+      </div>
+    </div>
+  );
 };
